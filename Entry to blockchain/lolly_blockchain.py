90898b03feef65c8d7edee68a0116b5fde107f2d
lohiths42 <49632021+lohiths42@users.noreply.github.com>
2020-07-25 07:28:38 +0530

    Update lolly_blockchain.py

--- Entry to blockchain/lolly_blockchain.py
+++ Entry to blockchain/lolly_blockchain.py
@@ -97,7 +97,7 @@ def is_valid():
     if is_valid:
         response = {'message': 'All good. The Blockchain is valid.'}
     else:
-        response = {'message': 'Houston, we have a problem. The Blockchain is not valid.'}
+        response = {'message': 'We have a problem. The Blockchain is not valid.'}
     return jsonify(response), 200
 
 # Running the app
